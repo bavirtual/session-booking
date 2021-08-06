634c15a298198d6a28ec1b752acee29dcf86c1dc
Mustafa Hajjar <mustafahajjar@hotmail.com>
2021-08-06 22:12:01 +0300

    Alpha 0.0.12 - access.php updates for beta testing

--- db/access.php
+++ db/access.php
@@ -22,19 +22,21 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 $capabilities = array(
 
     'local/booking:addinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_COURSE,
         'archetypes' => array(
-            'pluginbetausers' => CAP_ALLOW
+            'seniorinstructor' => CAP_ALLOW
         ),
     ),
 
     'local/booking:view' => array(
         'captype' => 'read',
-        'contextlevel' => CONTEXT_SYSTEM,
+        'contextlevel' => CONTEXT_COURSE,
         'archetypes' => array(
             'instructor' => CAP_ALLOW,
             'seniorinstructor' => CAP_ALLOW,
