de3b5778abfcb69a5dcff15bb1a824259526ac1a
Mustafa Hajjar <mustafahajjar@hotmail.com>
2023-05-15 21:23:04 +0300

    profile page display fix

--- profile.php
+++ profile.php
@@ -38,7 +38,6 @@ $categoryid = optional_param('categoryid', null, PARAM_INT);
 $courseid = optional_param('courseid', SITEID, PARAM_INT);
 $course = get_course($courseid);
 $userid = optional_param('userid', 0, PARAM_INT);
-$role = optional_param('role', 0, PARAM_INT);
 
 $url = new moodle_url('/local/booking/view.php');
 $url->param('courseid', $courseid);
@@ -46,7 +45,6 @@ $url->param('courseid', $courseid);
 $PAGE->set_url($url);
 
 $context = context_course::instance($courseid);
-$title = get_string('profile' . ($role?'instructor':'student'), 'local_booking');
 
 // basic access check
 require_login($course, false);
