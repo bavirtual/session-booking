b9bf1bf2391f117341244084f777b4fba43ddcb1
Mustafa Hajjar <mustafahajjar@hotmail.com>
2023-08-06 14:47:36 +0300

    externalapi AJAX exceptions

--- externallib.php
+++ externallib.php
@@ -28,6 +28,11 @@ defined('MOODLE_INTERNAL') || die;
 require_once($CFG->dirroot . '/group/lib.php');
 require_once($CFG->dirroot . '/local/booking/lib.php');
 
+use core_external\external_api;
+use core_external\external_value;
+use core_external\external_warnings;
+use core_external\external_single_structure;
+use core_external\external_function_parameters;
 use local_booking\external\logentry_exporter;
 use local_booking\local\logbook\forms\create as update_logentry_form;
 use local_booking\local\logbook\entities\logbook;
