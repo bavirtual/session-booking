8c18ccc712246a52ff13d4c95119601b10eb9885
Mustafa Hajjar <mustafahajjar@hotmail.com>
2023-08-06 16:27:06 +0300

    external_multiple_structure exception

--- externallib.php
+++ externallib.php
@@ -32,6 +32,7 @@ use core_external\external_api;
 use core_external\external_value;
 use core_external\external_warnings;
 use core_external\external_single_structure;
+use core_external\external_multiple_structure;
 use core_external\external_function_parameters;
 use local_booking\external\logentry_exporter;
 use local_booking\local\logbook\forms\create as update_logentry_form;
